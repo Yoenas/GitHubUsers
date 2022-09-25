6cf4c5e29ff32919773ddfbaf358bedda186fa73
yoenas <yusrilnurhadi63@gmail.com>
2022-09-25 17:02:37 +0700

    share image

--- app/src/main/java/com/yoenas/githubusers/DetailActivity.kt
+++ app/src/main/java/com/yoenas/githubusers/DetailActivity.kt
@@ -4,8 +4,8 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
-import android.os.Build
 import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES
 import android.os.Bundle
 import android.os.Parcelable
 import android.view.Menu
@@ -16,6 +16,8 @@ import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.drawable.toBitmap
 import com.bumptech.glide.Glide
 import com.yoenas.githubusers.databinding.ActivityDetailBinding
+import java.io.File
+import java.io.FileOutputStream
 
 class DetailActivity : AppCompatActivity() {
 
@@ -67,16 +69,12 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        return super.onSupportNavigateUp()
-    }
-
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.detail_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
+    @SuppressLint("DiscouragedApi")
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.action_menu_share -> {
@@ -88,9 +86,27 @@ class DetailActivity : AppCompatActivity() {
                         "Total ${dataUser?.repository} Repositories\n" +
                         "${dataUser?.follower} Followers & ${dataUser?.following} Following"
 
+                val imageResource: Int =
+                    resources.getIdentifier(dataUser?.avatar, null, packageName)
+                val bitmap = ResourcesCompat.getDrawable(resources, imageResource, null)?.toBitmap()
+                val outputFile = File(cacheDir, "${dataUser?.name}.png")
+                val outputStream = FileOutputStream(outputFile)
+                bitmap?.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                outputStream.flush()
+                outputStream.close()
+
                 val sendIntent: Intent = Intent().apply {
                     action = Intent.ACTION_SEND
                     putExtra(Intent.EXTRA_TEXT, textValue)
+                    putExtra(
+                        Intent.EXTRA_STREAM,
+                        if (SDK_INT >= VERSION_CODES.N) androidx.core.content.FileProvider.getUriForFile(
+                            this@DetailActivity,
+                            packageName,
+                            outputFile
+                        )
+                        else Uri.fromFile(outputFile)
+                    )
                     type = "text/plain"
                 }
 
--- app/src/main/java/com/yoenas/githubusers/SplashScreenActivity.kt
+++ app/src/main/java/com/yoenas/githubusers/SplashScreenActivity.kt
@@ -1,9 +1,11 @@
 package com.yoenas.githubusers
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
